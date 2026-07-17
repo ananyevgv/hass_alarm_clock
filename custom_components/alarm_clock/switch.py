cea6c5fd467ef6a026f2716c4974ece9819e4782
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-07-17 15:57:10 +0300

    Update switch.py

--- custom_components/alarm_clock/switch.py
+++ custom_components/alarm_clock/switch.py
@@ -1,42 +1,45 @@
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.entity import EntityCategory
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 import logging
 
 from .constants import DOMAIN
-from . import AlarmClockEntity
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    entities = []
+    """Set up the switch platform."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    entities.append(Active(coordinator))
-    async_add_entities(entities)
+    async_add_entities([ActiveSwitch(coordinator)])
     return True
 
 
-class Active(AlarmClockEntity, SwitchEntity):
+class ActiveSwitch(CoordinatorEntity, SwitchEntity):
+    """Representation of the alarm active switch."""
+
     def __init__(self, coordinator):
         super().__init__(coordinator)
-        self.set_id("active")
-        self.set_name("Active")
+        self._attr_unique_id = f"{coordinator.data['id']}_active"
+        self._attr_name = f"{coordinator.data['name']} Active"
+        self._attr_entity_category = EntityCategory.CONFIG
+        self._attr_icon = "mdi:alarm"
 
     @property
     def is_on(self):
+        """Return true if switch is on."""
         return self.coordinator.is_enabled
-
-    async def async_turn_on(self, **kwargs):
-        await self.coordinator.set_enabled(True)
-
-    async def async_turn_off(self, **kwargs):
-        await self.coordinator.set_enabled(False)
 
     @property
     def icon(self):
+        """Return the icon to use in the frontend."""
         return "mdi:alarm" if self.is_on else "mdi:alarm-off"
 
-    @property
-    def entity_category(self):
-        return EntityCategory.CONFIG
+    async def async_turn_on(self, **kwargs):
+        """Turn the switch on."""
+        await self.coordinator.set_enabled(True)
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the switch off."""
+        await self.coordinator.set_enabled(False)
